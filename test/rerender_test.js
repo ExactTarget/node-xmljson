4b9dfa282611a49ff8950776a09e85abb259e1d3
Adam Alexander <adamalex@gmail.com>
2013-04-28 20:20:41 -0400

    Add tests for arrays of arrays

--- test/rerender_test.js
+++ test/rerender_test.js
@@ -92,6 +92,36 @@ exports['translates between json and xml'] = {
 		});
 	},
 
+	'handles a top level array of arrays': function (test) {
+		test.expect(4);
+
+		var staticXml = '' +
+			'<data type="array">' +
+				'<item type="array"><item>val1</item><item>val2</item><item>val3</item></item>' +
+				'<item type="array"><item>val4</item><item>val5</item><item>val6</item></item>' +
+				'<item type="array"><item>val7</item><item>val8</item><item>val9</item></item>' +
+			'</data>';
+
+		var staticJson = '' +
+			'[' +
+				'["val1","val2","val3"],' +
+				'["val4","val5","val6"],' +
+				'["val7","val8","val9"]' +
+			']';
+
+		to_json(staticXml, function (error, data) {
+			test.ifError(error);
+			var json = JSON.stringify(data);
+			test.equal(json, staticJson, 'rendered JSON should be correct');
+
+			to_xml(json, function (error, xml) {
+				test.ifError(error);
+				test.equal(xml, staticXml, 'rendered XML should be correct');
+				test.done();
+			});
+		});
+	},
+
 	'handles a nested dictionary': function (test) {
 		test.expect(4);
 
@@ -186,6 +216,46 @@ exports['translates between json and xml'] = {
 					'{"objProp1":"objVal1"},' +
 					'{"objProp2":"objVal2"},' +
 					'{"objProp3":"objVal3"}' +
+				']' +
+			'}';
+
+		to_json(staticXml, function (error, data) {
+			test.ifError(error);
+			var json = JSON.stringify(data);
+			test.equal(json, staticJson, 'rendered JSON should be correct');
+
+			to_xml(json, function (error, xml) {
+				test.ifError(error);
+				test.equal(xml, staticXml, 'rendered XML should be correct');
+				test.done();
+			});
+		});
+	},
+
+	'handles an array of arrays': function (test) {
+		test.expect(4);
+
+		var staticXml = '' +
+			'<data>' +
+				'<array1 type="array">' +
+					'<item type="array">' +
+						'<item>objVal1</item><item>objVal2</item><item>objVal3</item>' +
+					'</item>' +
+					'<item type="array">' +
+						'<item>objVal4</item><item>objVal5</item><item>objVal6</item>' +
+					'</item>' +
+					'<item type="array">' +
+						'<item>objVal7</item><item>objVal8</item><item>objVal9</item>' +
+					'</item>' +
+				'</array1>' +
+			'</data>';
+
+		var staticJson = '' +
+			'{' +
+				'"array1":[' +
+					'["objVal1","objVal2","objVal3"],' +
+					'["objVal4","objVal5","objVal6"],' +
+					'["objVal7","objVal8","objVal9"]' +
 				']' +
 			'}';
 
